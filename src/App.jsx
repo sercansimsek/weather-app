11bd7d30e98502fd3e9b2baa5babfaeda1654eb3
sercansimsek <sercansimsek44@gmail.com>
2025-09-05 11:21:49 +0200

    Data fetched successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import { getWeatherData } from './data/api';
 
 import { FiveDayCard } from './components/5DayCard/FiveDayCard';
 import { DailyCard } from './components/DailyCard/DailyCard';
@@ -7,10 +6,21 @@ import { MainInfo } from './components/MainInfo/MainInfo';
 import { OtherCities } from './components/OtherCities/OtherCities';
 import { SearchBar } from './components/SearchBar/SearchBar';
 // import bcgImage from './assets/images/earth.png';
+const API_KEY = 'c36d433d08d3193950535e45b632cc5d';
 
 export const App = () => {
   const [weatherData, setWeatherData] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
+  const [query, setQuery] = useState('');
+
+  const BASE_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${API_KEY}&units=metric
+`;
+
+  const getWeatherData = async () => {
+    const response = await fetch(BASE_URL);
+
+    return response.json();
+  };
 
   const loadWeatherData = async () => {
     try {
@@ -18,20 +28,22 @@ export const App = () => {
       setWeatherData(data);
     } catch (error) {
       setErrorMessage('Failed to fetch weather data');
-      console.error('Error fetching weather data:', error);
-    }
+      console.error(errorMessage, error);
+    } 
   };
 
   useEffect(() => {
     loadWeatherData();
-  }, []);
+    setQuery('');
+    setErrorMessage('');
+  }, [query]);
 
-  console.log(weatherData, errorMessage);
-  
+  console.log(weatherData,);
+
   return (
     <div className="grid grid-cols-1 gap-10 bg-[url('../src/assets/images/earth.png')] object-cover bg-cover bg-no-repeat bg-[#030616] px-6 py-7 w-full min-h-screen md:grid-cols-2">
       <div className="md:col-span-2">
-        <SearchBar />
+        <SearchBar query={query} onSetQuery={setQuery} />
       </div>
       <MainInfo />
       <DailyCard />
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,33 +1,39 @@
-import searchIcon from "../../assets/images/Search.svg";
+import searchIcon from '../../assets/images/Search.svg';
 
-export const SearchBar = () => {
-	return (
-		<div className="flex gap-10 font-['Outfit'] justify-between items-center w-full">
-			<div className="flex gap-3 justify-center items-center bg-[#20293A] w-[240px] rounded-3xl text-[#F2F5F9] placeholder-[#F2F5F9] p-2">
-				<img src={searchIcon} alt="search Icon" className="h-6 w-6 cursor-pointer" />
-				<input
-					type="text"
-					placeholder="Search city...."
-					className="outline-none text-sm font-medium"
-				/>
-			</div>
+export const SearchBar = ({ query, onSetQuery }) => {
+  return (
+    <div className="flex gap-10 font-['Outfit'] justify-between items-center w-full">
+      <div className="flex gap-3 justify-center items-center bg-[#20293A] w-[240px] rounded-3xl text-[#F2F5F9] placeholder-[#F2F5F9] p-2">
+        <img
+          src={searchIcon}
+          alt="search Icon"
+          className="h-6 w-6 cursor-pointer"
+        />
+        <input
+          type="text"
+          placeholder="Search city...."
+          className="outline-none text-sm font-medium"
+          value={query}
+          onChange={(e) => onSetQuery(e.target.value)}
+        />
+      </div>
 
-			<div>
-				<div className="flex items-center justify-between h-[40px] w-[76px] bg-[#20293A] rounded-3xl p-1">
-					<button className="flex items-center justify-center h-8 w-8 bg-[#F2F5F9] rounded-full text-[#030616] font-bold text-base cursor-pointer">
-						&deg;C
-					</button>
-					<button className="flex items-center justify-center h-8 w-8 text-[#F2F5F9] font-bold text-base">
-						&deg;F
-					</button>
-				</div>
+      <div>
+        <div className="flex items-center justify-between h-[40px] w-[76px] bg-[#20293A] rounded-3xl p-1">
+          <button className="flex items-center justify-center h-8 w-8 bg-[#F2F5F9] rounded-full text-[#030616] font-bold text-base cursor-pointer">
+            &deg;C
+          </button>
+          <button className="flex items-center justify-center h-8 w-8 text-[#F2F5F9] font-bold text-base">
+            &deg;F
+          </button>
+        </div>
 
-				{/* <div className="flex items-center justify-end h-[40px] w-[76px] bg-[#20293A] rounded-3xl">
+        {/* <div className="flex items-center justify-end h-[40px] w-[76px] bg-[#20293A] rounded-3xl">
 					<div className="flex items-center justify-center h-8 w-8 bg-[#20293A] rounded-full text-[#F2F5F9] font-bold text-base">
 						&deg;F
 					</div>
 				</div> */}
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 };
